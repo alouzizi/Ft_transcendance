1d24a27d106da20d57d05e37bb2f57dcdf69a20e
salihalammari <salihalammari91@gmail.com>
2023-09-29 12:45:30 +0100

    setup project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UsersModule, BookmarkModule, PrismaModule],
   controllers: [],
   providers: [],
 })
