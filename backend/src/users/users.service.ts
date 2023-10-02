47caf1e693fdbebb540a71825f2d33fb0e8f0d8e
salihalammari <salihalammari91@gmail.com>
2023-10-02 22:55:59 +0100

    add dto and fixed user

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+
+export class UsersService {
+    constructor(private prisma: PrismaService){}
+
+    async create();
+}
