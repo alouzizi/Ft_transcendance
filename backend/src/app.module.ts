8c19bdc20ddcf269d63a6686579fd6da2591fe06
salihalammari <salihalammari91@gmail.com>
2023-10-06 00:44:21 +0100

    create auth with jwt and users validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, UsersModule, PrismaModule],
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Get, Post, Request, Response } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Req() req:Request) {
-        // console.log(req);
-        return this.authService.signup();
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
-    @Post('signin')
-    signin() {
-        return this.authService.signin();
-    }
+  @Post('signin')
+  async signin(@Request() req, @Response() res, @Body() dto: AuthDto) {
+    return this.authService.signin(dto, req, res);
+  }
+
+  @Get('signout')
+  signout(@Request() req, @Response() res) {
+    return this.authService.signout(req, res);
+  }
 }
 
 
