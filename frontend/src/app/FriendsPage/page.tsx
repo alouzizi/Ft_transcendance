7b4dccb05a69f3f223f49beec099c00dc2f9f32d
hixcoder <hixcoder@gmail.com>
2023-10-09 09:58:06 +0100

    finish the front of the friendPage

--- frontend/src/app/FriendsPage/page.tsx
+++ frontend/src/app/FriendsPage/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import FriendItem from "@/components/FriendsComp/FriendItem";
 import { useState } from "react";
 
 export default function FriendsPage() {
@@ -10,6 +11,9 @@ export default function FriendsPage() {
     false,
     false,
   ]);
+
+  // this is for the status of the friend items shown (onlin,all...)
+  const [itemsStatus, setItemsStatus] = useState("Online");
 
   let friendBtnList = [
     {
@@ -31,43 +35,57 @@ export default function FriendsPage() {
   ];
 
   function getIconStyle(index: number) {
-    const iconStyle = ` mr-4 font-bold text-md py-1 px-4 rounded-md  
+    const iconStyle = ` font-bold  py-1 rounded-md  
     transition ease-in-out delay-100
     ${
       isSelectedList[index]
         ? "text-white bg-[#515562] hover:bg-[#515562] hover:text-white"
         : "text-gray-300 bg-transparent hover:bg-[#2A2F40] hover:text-slate-50"
     }
+
+    // small screen
+    text-xs px-2 mr-1.5
+    // big screen 
+    md:text-sm lg:text-md lg:px-4  md:mr-4
+    
       `;
     return iconStyle;
   }
 
-  const handleItemClick = (index: number) => {
+  const handleItemClick = (item: { btnName: string; index: number }) => {
     // Create a copy of isSelectedList and toggle the state for the clicked item
     const updatedIsSelectedList = [];
     for (let i = 0; i < isSelectedList.length; i++) {
       updatedIsSelectedList[i] = false;
     }
-    updatedIsSelectedList[index] = true;
+    updatedIsSelectedList[item.index] = true;
+    setItemsStatus(item.btnName);
     setIsSelectedList(updatedIsSelectedList);
-    console.log(updatedIsSelectedList[index]);
+    console.log(updatedIsSelectedList[item.index]);
   };
 
   return (
-    <div className="flex flex-col bg-color-main h-screen w-screen  ">
+    <div className="flex flex-col bg-color-main h-screen w-screen ">
       <div className="flex flex-col justify-end mt-12 mb-8 ml-8  md:ml-24 ">
         <h1 className="text-left font-bold text-lg md:text-2xl mb-12">
           Friends
         </h1>
+        {/* this is the head of the FriendPage */}
         <div
-          className=" flex flex-row justify-around ml-0  w-[50%]   h-fit mb-12 max-w-5xl
+          className=" flex flex-row justify-start ml-0  h-fit mb-8 max-w-5xl
+
+          // small screen
+          w-[80%]
+          // big screen 
+          lg:w-[60%]
+
         "
         >
-          {friendBtnList.slice(0, 5).map((item) => (
+          {friendBtnList.map((item) => (
             <button
               key={`friendBtn-${item.index}`}
               onClick={() => {
-                handleItemClick(item.index);
+                handleItemClick(item);
               }}
               className={getIconStyle(item.index)}
             >
@@ -75,9 +93,43 @@ export default function FriendsPage() {
             </button>
           ))}
 
-          <button className="mr-4 w-fit font-meduim text-md py-1 px-3 rounded-md   bg-green-700 hover:bg-green-600">
+          <button
+            className="mr-4 w-fit font-meduim  py-1 rounded-md   bg-green-700 hover:bg-green-600
+          
+         
+          // small screen
+          text-xs px-2
+          // big screen 
+          md:text-sm lg:text-md lg:px-4 
+          "
+          >
             Add Friend
           </button>
+        </div>
+        {/* this is the friend items */}
+        <div
+          className="flex flex-col  max-w-3xl
+        
+        // small screen
+          w-[80%]
+          // big screen 
+          lg:w-[60%]
+        "
+        >
+          <p
+            className="mr-4 mb-4 w-fit font-bold  py-1 px-3 text-gray-400 
+          
+          // small screen
+          text-sm
+          // big screen 
+          md:text-md 
+          "
+          >
+            {`${itemsStatus} - 3`}
+          </p>
+          <FriendItem friendImg={"profile-img.png"} />
+          <FriendItem friendImg={"profile-img.png"} />
+          <FriendItem friendImg={"profile-img.png"} />
         </div>
       </div>
     </div>
