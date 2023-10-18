800d21aab2a594ef11dce1c1e187ff85b876849a
salihalammari <salihalammari91@gmail.com>
2023-10-18 17:17:46 +0100

    delete file and create clean code with strategy

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from './user.service';
+
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-  ) {}
+  constructor() {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userService.findUser(username);
+  async register(user: { username: string; email: string; password: string }) {
+    const createdUser = await this.prisma.user.create({
+      data: user,
+    });
+    return createdUser;
+  }
 
+  async validateUser(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
     if (user && user.password === password) {
       return user;
     }
-
     return null;
-  }
-
-  async login(user: User) {
-    const payload = { username: user.username, sub: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
   }
 }
--- backend/src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const jwtSecret = process.env.JWT_SECRET;
-export const clientUrl = process.env.CLIENT_URL;
