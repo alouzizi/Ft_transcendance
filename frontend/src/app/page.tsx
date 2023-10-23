7d00a76983645f699a927c991071e7aab76c8e99
salihalammari <salihalammari91@gmail.com>
2023-10-23 13:24:08 +0100

    add readme

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import RegistrationComponent from '../components/RegistrationComponent';
 export default function Home() {
   return (
    <div>
+     <div className='h-screen flex items-center justify-center'></div>
     <RegistrationComponent />
    </div>
   )
--- frontend/src/pages/api/auth/index.js
+++ frontend/src/pages/api/auth/index.js
@@ -3,6 +3,7 @@ import RegistrationComponent from '../components/RegistrationComponent';
 export default function Home() {
   return (
     <div>
+      <div className="bg-red"></div>
       <RegistrationComponent />
       {/* Add other components, including a LoginComponent */}
     </div>
