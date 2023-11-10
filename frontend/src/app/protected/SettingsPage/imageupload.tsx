59e2fa8c998b3e706a783fcc7810c9bac922436e
salihalammari <salihalammari91@gmail.com>
2023-11-10 20:53:48 +0100

    add image upload

--- frontend/src/app/protected/SettingsPage/imageupload.tsx
+++ frontend/src/app/protected/SettingsPage/imageupload.tsx
@@ -1,11 +1,14 @@
 "use client";
 // components/ImageUpload.tsx
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import { useState } from "react";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { useEffect, useState } from "react";
 import Avatar from "public/DefaultAvatar.png";
 import Image from "next/image";
+import { useGlobalContext } from "@/app/context/store";
+import { IconButton } from "@mui/material";
 
 const ImageUpload = () => {
+  const { user } = useGlobalContext();
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,32 +22,35 @@ const ImageUpload = () => {
       reader.readAsDataURL(file);
     }
   };
-
+  useEffect(() => {
+    console.log("user --> ", user);
+  }, [user.id]);
   return (
-    <div className="border ml-4 -mt-16">
-<div className='border border-yellow-500'>
-
-      <AddPhotoAlternateIcon style={{ color: 'white' }}>
-        </AddPhotoAlternateIcon>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-        id="image-upload"
-        style={{ display: "none" }}
+    <div className="kborder ml-4 -mt-24">
+      <IconButton className="z-50 relative top-10 left-11">
+        <label htmlFor="image-upload">
+          <input
+            className="border-4 border-pink-500"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            id="image-upload"
+            style={{ display: "none" }}
+          />
+          <div className="bg-slate-50 rounded-full h-8 w-8 flex items-center justify-center">
+          <AddPhotoAlternateIcon style={{ color: "black" }} />
+          </div>
+        </label>
+      </IconButton>
+      <div>
+        <Image
+          width={100}
+          height={100}
+          src={selectedImage || Avatar}
+          alt="Preview"
+          className="w-20 h-20 rounded-full bg-cover object-contain"
         />
-        </div>
-      <label htmlFor="image-upload">
-        <div>
-          <Image
-            width={100}
-            height={100}
-            src={selectedImage || Avatar}
-            alt="Preview"
-            className="w-20 rounded-full"
-            />
-        </div>
-      </label>
+      </div>
     </div>
   );
 };
--- frontend/src/app/protected/SettingsPage/page.tsx
+++ frontend/src/app/protected/SettingsPage/page.tsx
@@ -5,8 +5,10 @@ import ImageUpload from "./imageupload";
 export default function SettingsPage() {
   return (
     <>
-      <form className="flex border border-green-500 flex-col justify-center gap-6 w-[500px]">
-        <div className="border border-red-500 min-w-[400px] h-40 rounded-2xl flex flex-col items-center w-full pt-10 gap-14"></div>
+      <form className="flex bg-slate-400 border border-green-500 flex-col justify-center gap-6 w-[500px]">
+        <div className="border border-red-500 min-w-[400px] h-40 rounded-2xl flex flex-col items-center w-full pt-10 gap-14">
+
+        </div>
         <ImageUpload/>
         <div className="flex flex-col w-full border">
           <input
