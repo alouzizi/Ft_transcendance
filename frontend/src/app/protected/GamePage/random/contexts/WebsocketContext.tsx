cbbe6fead5175269ab61a74cbbbdea356b30ead9
hixcoder <hixcoder@gmail.com>
2023-11-18 11:46:49 +0100

    now docker is running

--- frontend/src/app/protected/GamePage/random/contexts/WebsocketContext.tsx
+++ frontend/src/app/protected/GamePage/random/contexts/WebsocketContext.tsx
@@ -1,5 +1,5 @@
 // import { createContext } from 'react';
-// import { io, Socket } from 'Socket.IO-client';
+// import { io, Socket } from 'socket.io-client';
 
 // export const socket: Socket = io('http://localhost:4000');
 // export const WebsocketContext = createContext<Socket>(socket);
